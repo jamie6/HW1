c012b785efd4e6c008f0fa272965aebc1af7638f
Jamie Lopez <jamielopez@cpp.edu>
2018-08-28 17:02:18 -0700

    Changed output to Hello, my name is Jamie Lopez

--- hwOne.java
+++ hwOne.java
@@ -2,6 +2,12 @@ class hwOne
 {
 	public static void main( String[] args )
 	{
-		System.out.println("Hello World");
+		System.out.println("Hello, my name is Jamie Lopez");
 	}
 }
+
+/*
+"Hello World"
+"Hello, my name is Jamie Lopez"
+"Goodbye, I finished my CS 3700 HW1"
+*/
